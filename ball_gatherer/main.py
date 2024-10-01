0c91a10a3cc9d9539f90bf740672d290f4b7afeb
TH3W4TCHM3N <thewatchmen.game@outlook.com>
2024-10-01 10:36:45 +0200

    custom ip & main started

--- ball_gatherer/main.py
+++ ball_gatherer/main.py
@@ -5,12 +5,25 @@ from robomaster import camera
 import time
 import cv2
 import numpy as np
+from utils import get_ip_starting_with
 
-config.LOCAL_IP_STR = "192.168.5.87"
+# Initialize Connection
+ip_prefix = '192.168.5.'
 config.ROBOT_IP_STR = "192.168.5.3"
+config.LOCAL_IP_STR = get_ip_starting_with(ip_prefix)
 ep_robot = robot.Robot()
-
 ep_robot.initialize(conn_type="sta", sn="3JKCK5C00308HQ")
 
-ep_version = ep_robot.get_version()
-print("Robot Version: {0}".format(ep_version))
+
+# Main function
+def main():
+    try:
+        # Robot Commands
+        ep_version = ep_robot.get_version()
+        print("Robot Version: {0}".format(ep_version))
+    finally:
+        # Close the connection when done
+        ep_robot.close()
+
+if __name__ == '__main__':
+    main()
---
+++ ball_gatherer/utils.py
@@ -0,0 +1,23 @@
+from robomaster import config
+from robomaster import robot
+import socket
+
+def get_ip_starting_with(prefix):
+    """
+    Get the local IP address that starts with the given prefix.
+    
+    Args:
+    prefix (str): The prefix to filter the IP addresses by (e.g., '192.168.5.')
+    
+    Returns:
+    str: The first local IP address that starts with the prefix, or None if not found.
+    """
+    hostname = socket.gethostname()
+    addresses = socket.getaddrinfo(hostname, None)
+    
+    for address in addresses:
+        ip = address[4][0]
+        if ip.startswith(prefix):
+            return ip
+    
+    return None  # Return None if no matching IP address is found
